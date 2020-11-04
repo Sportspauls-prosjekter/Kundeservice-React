219ac9f238dba40d80938964679f7d88ec093719
Paul Høglend <pmhoglend@gmail.com>
2020-11-04 14:30:35 +0100

    Grunnleggende CRUD i Controller

--- Kundeservice/Controllers/ServiceController.cs
+++ Kundeservice/Controllers/ServiceController.cs
@@ -1,4 +1,6 @@
-﻿using Kundeservice.DAL;
+﻿using Castle.Core.Internal;
+using Kundeservice.DAL;
+using Kundeservice.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -26,9 +28,82 @@ namespace Kundeservice.Controllers
         [HttpGet]
         public async Task<ActionResult> HentAlle()
         {
-            List<Faqs> Sporsmaalene = await _db.HentAlle();
-            _log.LogInformation($"{Ok(Sporsmaalene)}");
-            return Ok(Sporsmaalene);
+            List<Faqs> sporsmaalene = await _db.HentAlle();
+            if(sporsmaalene.IsNullOrEmpty())
+            {
+                _log.LogWarning($"{Ok(sporsmaalene)}");
+                return NotFound("Ingen Spørsmål funnet");
+            }
+            _log.LogInformation($"{Ok(sporsmaalene)}");
+            return Ok(sporsmaalene);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> HentEn(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Faq faqen = await _db.HentEn(id);
+                if (faqen == null)
+                {
+                    _log.LogInformation("Fant ikke spørsmålsobjektet");
+                    return NotFound("Fant ikke spørsmålsobjektet");
+                }
+                _log.LogInformation($"{Ok(faqen)}");
+                return Ok(faqen);
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Lagre(Faq innFaq)
+        {
+            if (ModelState.IsValid)
+            {
+                bool returOK = await _db.Lagre(innFaq);
+                if (!returOK)
+                {
+                    _log.LogInformation("Spørsmålsobjektet kunne ikke lagres!");
+                    return BadRequest("Spørsmålsobjektet kunne ikke lagres!");
+                }
+                return Ok(); 
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Spørsmålsobjektet kunne ikke lagres!");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Endre(Faq innFaq)
+        {
+            if (ModelState.IsValid)
+            {
+                bool returOK = await _db.Endre(innFaq);
+                if (!returOK)
+                {
+                    _log.LogInformation("FAQ ble ikke funnet");
+                    return NotFound("FAQ ble ikke funnet");
+                }
+                _log.LogInformation("Endringen av FAQ-objektet ble utført");
+                return Ok("Endringen av FAQ-objektet ble utført");
+            }
+            _log.LogWarning("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Slett(int id)
+        {
+            bool returOK = await _db.Slett(id);
+            if (!returOK)
+            {
+                _log.LogWarning("Sletting av spørsmålet ble ikke utført");
+                return NotFound("Sletting av spørsmålet ble ikke utført");
+            }
+            _log.LogInformation("Sletting av spørsmålet ble utført");
+            return Ok("Sletting av spørsmålet ble utført");
+
+        }
+
     }
 }
