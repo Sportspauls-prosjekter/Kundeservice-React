7d1f9526338cdb1f648ae428f4798f9c9af9fe2b
Paul Høglend <pmhoglend@gmail.com>
2020-11-07 14:54:08 +0100

    Begynt på kategoriknapper

--- Kundeservice/ClientApp/src/components/Home.jsx
+++ Kundeservice/ClientApp/src/components/Home.jsx
@@ -3,52 +3,23 @@ import axios from "axios";
 import '../Style/home.css';
 
 export default class Home extends Component {
+    
+
     state = {
-       faqs: []
+        faqs: []
     }
 
-    constructor() {
-        super();
-        this.faqs = { faqs: [] };
-    }  
-
-
-    componentDidMount() {
-        axios.get(`https://localhost:44383/api/service`)
-            .then(res => {
-                const faqs = res.data;
-                this.setState({ faqs });
-            })
+    async componentDidMount() {
+        const { data : faqs } = await axios.get(`https://localhost:44383/api/service`);
+        this.setState({faqs})
     }
-
-   
-
 
     render() {
+        const kategoriene = ["Reise", "Ordre", "Ruter", "Innlogging"]
         return (
             <div id="faq">
                 <h2> Velkommen til NOR-WAY kundeservice </h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Spørsmål</th>
-                            <th>Svar</th>
-                            <th>Kategori</th>
-                            <th>Rating</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            {this.state.faqs.map(question => (
-                                <tr key={question.id}>
-                                    <td>{question.sporsmaal}</td>
-                                    <td>{question.svar}</td>
-                                    <td>{question.kategori}</td>
-                                    <td>{question.rating}</td>
-                                </tr>)
-                            )
-                        } 
-                   </tbody>
-            </table>
+                {kategoriene.map(kat => <div className="kategori" key={ kategoriene.indexOf(kat)}> <p>{kat}</p> </div>)}
             </div>
         );
     }
